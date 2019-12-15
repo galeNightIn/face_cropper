725e73fd423ab9ff8cd4d5f71cd6e9487ae5b5a4
galeNightIn <nightingale.alex.info@gmail.com>
2019-12-15 06:17:46 +0300

    Fix paths

--- face_cropper/core/model.py
+++ face_cropper/core/model.py
@@ -1,6 +1,18 @@
+import logging
+import os
+from pathlib import Path
+
 import cv2
 
 __all__ = ['NetModel']
+
+logger = logging.getLogger(__file__)
+
+PATH_DIR = Path('.')
+logger.info(f'Path dir {PATH_DIR!r}')
+
+PATH_DIR_PARENT = str(PATH_DIR.parent)
+logger.info(f'Path dir parent {PATH_DIR_PARENT!r}')
 
 
 class NetModel:
@@ -10,12 +22,12 @@ class NetModel:
         self._net = None
 
         if dnn_source == "RES10":
-            self._model_file = "../models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-            self._config_file = "../models/deploy.prototxt"
+            self._model_file = os.path.join(PATH_DIR_PARENT, "models/res10_300x300_ssd_iter_140000_fp16.caffemodel")
+            self._config_file = os.path.join(PATH_DIR_PARENT, "models/deploy.prototxt")
             self._net = cv2.dnn.readNetFromCaffe(self._config_file, self._model_file)
         else:
-            self._model_file = "../models/opencv_face_detector_uint8.pb"
-            self._config_file = "../models/opencv_face_detector.pbtxt"
+            self._model_file = os.path.join(PATH_DIR_PARENT, "models/opencv_face_detector_uint8.pb")
+            self._config_file = os.path.join(PATH_DIR_PARENT, "models/opencv_face_detector.pbtxt")
             self._net = cv2.dnn.readNetFromTensorflow(self._model_file, self._config_file)
 
     @property
