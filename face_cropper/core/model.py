25ef231d3bd1fbf3d5b91f6041b2b5e1a68dcf6e
galeNightIn <nightingale.alex.info@gmail.com>
2019-12-15 06:08:42 +0300

    Fix paths

--- face_cropper/core/model.py
+++ face_cropper/core/model.py
@@ -10,12 +10,12 @@ class NetModel:
         self._net = None
 
         if dnn_source == "RES10":
-            self._model_file = "models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-            self._config_file = "models/deploy.prototxt"
+            self._model_file = "../models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+            self._config_file = "../models/deploy.prototxt"
             self._net = cv2.dnn.readNetFromCaffe(self._config_file, self._model_file)
         else:
-            self._model_file = "models/opencv_face_detector_uint8.pb"
-            self._config_file = "models/opencv_face_detector.pbtxt"
+            self._model_file = "../models/opencv_face_detector_uint8.pb"
+            self._config_file = "../models/opencv_face_detector.pbtxt"
             self._net = cv2.dnn.readNetFromTensorflow(self._model_file, self._config_file)
 
     @property
