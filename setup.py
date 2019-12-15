1342bf3dd88fa8be0614cc4fdc62661b31a58245
galeNightIn <nightingale.alex.info@gmail.com>
2019-12-15 06:05:07 +0300

    Add package data

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name='face_cropper',
     version=get_version(),
     packages=find_packages(),
-    package_data={'face_cropper': ['*.caffemodel', '*.pb', '*.pbtxt', '*.prototxt']},
+    package_data={'face_cropper': ['models/*.caffemodel', 'models/*.pb', 'models/*.pbtxt', 'models/*.prototxt']},
     url='https://github.com/galeNightIn/face_cropper',
     description='Face cropper module',
     long_description=readme(),
